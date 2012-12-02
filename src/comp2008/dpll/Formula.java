48fc0a1242dc361e2b0a35a5685281268691bbec
Hio Leong <hiowengleong@gmail.com>
2012-12-02 21:38:43 +0000

    Added findUnit function.

--- src/comp2008/dpll/Formula.java
+++ src/comp2008/dpll/Formula.java
@@ -2,20 +2,35 @@ package comp2008.dpll;
 
 import java.util.List;
 
-public class Formula {	
-	
+public class Formula {
+
 	private int n; // number of distinct literals
 	private int m; // number of clauses
 	private List<Clause> clauses;
-	
-	public Formula() {
+
+	public Formula() { 
 		m = 0;
 		n = 0;
 	}
-	
+
 	public Formula(int n, int m) {
 		this.n = n;
 		this.m = m;
+	}
+
+	/**
+	 * Method finding a clause which contains one literal, if found, the literal
+	 * is returned, otherwise -1 is returned.
+	 * 
+	 * @return the unit, or -1 if not found.
+	 */
+	public int findUnit() {
+		for (Clause c : clauses) {
+			if (c.size() == 1) {
+				return c.getFirstLiteral();
+			}
+		}
+		return -1;
 	}
 
 }
